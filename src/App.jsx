dddb44ecaf694aba9004aad12ecd91bca534269b
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2026-03-10 12:08:19 +0530

    updated1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import Naver from './Components/Naver'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Index from './Components/Index';
  
  const App = () => {
    return (
      <div>
        <Naver/>
+       <Index/>
      </div>
    )
  }
--- src/Components/Naver.jsx
+++ src/Components/Naver.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Link from 'react-bootstrap/Navbar';
+import image1 from '../assets/imgi_1_logo.png'
 
 const Naver = () => {
   return (
     <div>
      <Navbar className='navhead' data-bs-theme="dark" >
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home"><img src={image1} alt="" className='img1'/></Navbar.Brand>
           <Nav className="me-auto nav1">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home" className='nav2'>Home</Nav.Link>
+            <Link href="#features"className='nav2'>Features</Link>
+            <Link href="#pricing" className='nav2'>Pricing</Link>
+            <Link href="#pricing" className='nav2'>Contact</Link>
+            
           </Nav>
         </Container>
       </Navbar>
