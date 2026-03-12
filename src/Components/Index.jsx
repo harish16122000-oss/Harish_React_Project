f54ac7de678697055e52522678c122f1a82ce749
harish16122000-oss <harish16122000@gmail.com>
2026-03-12 21:16:29 +0530

    up

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 
 import Card from 'react-bootstrap/Card';
- 
+ import { TbTruckDelivery } from "react-icons/tb";
+ import { SiFreecad } from "react-icons/si";
+ import { FaAward } from "react-icons/fa";
 const Index = () => {
    const a=  [
-    {id:1, head:"Fast Delivery", text:"Fast and reliable delivery Right to your doorstep"},
-    {id:2, head:"Free Shipping", text:"Free shipping on all orders No extra charges"},
-    {id:3, head:"Best Quality", text:"Premium quality products Trusted and long-lasting"},
+    {id:1, image:<TbTruckDelivery />, head:"Fast Delivery", text:"Fast and reliable delivery Right to your doorstep"},
+    {id:2, image:<SiFreecad />, head:"Free Shipping", text:"Free shipping on all orders No extra charges"},
+    {id:3, image:<FaAward />, head:"Best Quality", text:"Premium quality products Trusted and long-lasting"},
 
   ];
   return (
@@ -38,22 +40,25 @@ const Index = () => {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-    <div>
-      <div className='h1 text-center mt-5 blog'>Why Shop With Us <span className='blog1'>__</span> </div>
+    <div className='container'> 
+    <div className='blog0'>
+      <div className='h1 text-center mt-5 blog '>Why Shop With Us <span className='blog1'>__</span> </div>
        </div>
-<div className='mt-5 pt-md-5 d-md-flex justify-content-center card0'>
+<div className='mt-5 pt-md-5 d-md-flex justify-content-center card0 '>
 {a.map((ab)=>(
-      <Card style={{ width: '18rem' }}className='text-center  card1'key={ab.id}>
+      <Card style={{ width: '18rem' }}className='text-center  
+      card1 w-100'key={ab.id}>
       <p className='mt-5'></p>
-     
+     <i className='icon'>{ab.image}</i>
       <Card.Body>
-        <Card.Title>{ab.head}</Card.Title>
-        <Card.Text>
+        <Card.Title className='h1'>{ab.head}</Card.Title>
+        <Card.Text className='mt-1'>
           {ab.text}
         </Card.Text>
        
       </Card.Body>
     </Card>))}
+    </div>
     </div>
     </div>
   )
